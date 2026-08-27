ef29afa55caf30036d039109efd05306c1231ac3
Amit Jain <a3itj@users.noreply.github.com>
2026-08-27 08:40:22 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxWater = 0;
+        let left = 0;
+        let right = heights.length - 1;
+
+        while (left < right) {
+            const width = right - left;
+            const currentHeight = Math.min(heights[left], heights[right]);
+            
+            maxWater = Math.max(maxWater, width * currentHeight);
+
+            // Move the pointer that points to the shorter wall
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+
+    }
+}
